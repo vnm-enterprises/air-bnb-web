14ce7ead28ba403034eba830a3e4617222316aad
vihanga <vihanganethusara00@gmail.com>
2026-02-08 15:37:01 +0530

    refactor : auth actions dark theme rmoval

--- components/layout/AuthActions.tsx
+++ components/layout/AuthActions.tsx
@@ -1,12 +1,20 @@
 export default function AuthActions() {
   return (
     <>
-      <button className="hidden sm:flex cursor-pointer text-sm font-bold text-slate-700 dark:text-slate-300 px-6 py-2.5 rounded-full hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
+    <button className="hidden sm:flex border border-[#2C5F5D] text-[#ffffff] text-sm cursor-pointer font-bold px-6 py-2.5 rounded-lg shadow-lg shadow-[#2C5F5D]/20 hover:shadow-[#2C5F5D]/30 transition-all active:scale-95">
         Log In
       </button>
-      <button className="hidden sm:flex bg-[#2C5F5D] text-white text-sm cursor-pointer font-bold px-6 py-2.5 rounded-full shadow-lg shadow-[#2C5F5D]/20 hover:shadow-[#2C5F5D]/30 transition-all active:scale-95">
+      <button
+        className="hidden sm:flex text-sm font-semibold px-6 py-2.5 rounded-lg"
+        style={{
+          backgroundColor: "#2E5E59",
+          color: "#FFFFFF",
+        }}
+      >
         Sign Up
+
       </button>
+
     </>
   );
 }
