ef6b56332dd65e653896f564f69b26e9a6f5ff07
vihanga <vihanganethusara00@gmail.com>
2026-02-08 15:36:30 +0530

    Refactor: Navbar to remove dark theme

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,12 +1,11 @@
 export default function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <div className="text-[#2C5F5D]">
+      <div style={{ color: "#EAF2F1" }}>
         <svg
           className="w-9 h-9"
           fill="none"
           viewBox="0 0 48 48"
-          xmlns="http://www.w3.org/2000/svg"
         >
           <path
             d="M4 4H17.3334V17.3334H30.6666V30.6666H44V44H4V4Z"
@@ -14,8 +13,11 @@ export default function Logo() {
           ></path>
         </svg>
       </div>
-      <h1 className="text-2xl font-extrabold tracking-tight text-slate-900 dark:text-white">
-        StayFound
+      <h1
+        className="text-xl font-semibold tracking-tight"
+        style={{ color: "#FFFFFF" }}
+      >
+        LOGO
       </h1>
     </div>
   );
