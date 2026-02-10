ab0f314a506234049c80792f31f20e9200787191
vihanga <vihanganethusara00@gmail.com>
2026-02-10 23:46:39 +0530

    feat: breadcrumbs

---
+++ components/search/Breadcrumbs.tsx
@@ -0,0 +1,11 @@
+export default function Breadcrumbs() {
+  return (
+    <div className="px-6 py-3 my-5 flex items-center gap-2 text-[10px] font-bold text-slate-400 uppercase tracking-widest">
+      <span className="hover:text-primary cursor-pointer">Home</span>
+      <span>›</span>
+      <span className="hover:text-primary cursor-pointer">United Kingdom</span>
+      <span>›</span>
+      <span className="text-slate-700 ">London</span>
+    </div>
+  );
+}
