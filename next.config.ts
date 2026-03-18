7a25c654a319783ae92af0ef21238cf07ab26f72
vihanga <vihanganethusara00@gmail.com>
2026-03-18 23:16:02 +0530

    Add/update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.wp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.wordpress.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
