c50bb4582be447991c808e1c98db4d1275517022
vihanga <vihanganethusara00@gmail.com>
2026-03-26 00:13:25 +0530

    Add/update domain/models/property.ts

---
+++ domain/models/property.ts
@@ -0,0 +1,11 @@
+export type {
+  Property,
+  PropertyResponse,
+  PropertiesResponse,
+  AvailabilityResponse,
+  UnavailableDateRange,
+  UnavailableDatesResponse,
+  GetPropertiesParams,
+  DeletePropertyResponse,
+  UploadPropertyImagesResponse,
+} from "@/lib/propertyApi";
