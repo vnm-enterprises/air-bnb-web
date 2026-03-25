2628e744152917b5b9a612dee626144ac401182b
vihanga <vihanganethusara00@gmail.com>
2026-03-26 00:13:25 +0530

    Add/update infrastructure/services/index.ts

---
+++ infrastructure/services/index.ts
@@ -0,0 +1,7 @@
+export { authService } from "./auth-service";
+export { bookingService } from "./booking-service";
+export { paymentService } from "./payment-service";
+export { propertyService } from "./property-service";
+export { reviewService } from "./review-service";
+export { supportService } from "./support-service";
+export { wishlistService } from "./wishlist-service";
